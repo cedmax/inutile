ca74f425a5823040964aa99714b5b35efb27c5ae
Marco Cedaro <cedmax@users.noreply.github.com>
2018-03-09 20:18:08 +0000

    added levels

--- src/Components/OfflineMain.js
+++ src/Components/OfflineMain.js
@@ -1,10 +1,8 @@
 import React, { Fragment, Component } from 'react'
 import styled from 'styled-components'
 
-const winner = 500
-
 const Ball = styled.div`
-  animation: moveX 4.05s linear 0s infinite alternate, moveY 4.4s linear 0s infinite alternate;
+  animation: moveX ${props => `${props.speed}.05s`} linear 0s infinite alternate, moveY ${props => `${props.speed}.4s`} linear 0s infinite alternate;
   border-radius: 50px;
   background: red;
   width: 5vw;
@@ -26,31 +24,64 @@ export default class Offline extends Component {
     super(props)
 
     this.state = {
+      winner: 500,
       score: 0,
+      speed: 5,
+      running: true,
+      next: true,
     }
+    this.speedUp = this.speedUp.bind(this)
     this.gotIt = this.gotIt.bind(this)
   }
 
+  speedUp () {
+    const { winner: oldWinner, speed: oldSpeed } = this.state
+    const winner = oldWinner + 500
+    const speed = oldSpeed - 1
+
+    if (speed > 2) {
+      this.setState({
+        running: true,
+        winner,
+        speed,
+      })
+    } else {
+      this.setState({ next: false })
+    }
+  }
+
   gotIt () {
+    const { score: oldScore, winner } = this.state
+    const score = oldScore + 100
+    const running = score < winner
+
     this.setState({
-      score: this.state.score + 100,
+      score,
+      running,
     })
   }
 
   render () {
-    const { score } = this.state
+    const { score, speed, running, next } = this.state
 
     return (
       <Fragment>
         <Score>{score}</Score>
-        { score < winner ? (
+        { running ? (
           <Fragment>
-            <Ball onClick={this.gotIt} />
+            <Ball onClick={this.gotIt} speed={speed} />
             <p>You're now offline</p>
             <p>There isn't much point in linking other web resources</p>
             <p>Play a game instead: catch the ball!</p>
           </Fragment>
-        ) : <p>YOU WON!</p> }
+        ) : (
+          <Fragment>
+            <p>
+              { next ? <input onClick={this.speedUp} type="button" value="NEXT" /> : 'YOU WON!'}
+            </p>
+          </Fragment>
+        )
+        }
       </Fragment>
     )
   }
