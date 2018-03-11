b692f44e434791fea4d83a666daacab6765f47a6
Marco Cedaro <cedmax@users.noreply.github.com>
2018-03-11 22:26:11 +0000

    duh

--- static.config.js
+++ static.config.js
@@ -29,6 +29,7 @@ export default {
             <title>Inutile dot co</title>
             <meta name="description" content="Useless, unnecessary, pointless" />
             <meta name="author" content="cedmax" />
+            <link rel="manifest" href="/manifest.json" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link href="favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
             <link href="favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
